8f6743e2e44b8987d93d76107857041ed5a8ab2f
sdrvsavva-coder <sdrvsavva@gmail.com>
2026-05-16 17:45:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -33,6 +33,10 @@ class Card(db.Model):
     
 
 #Задание №1. Создать таблицу User
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
 
 
 #Запуск страницы с контентом
@@ -44,11 +48,18 @@ def login():
         form_password = request.form['password']
             
         #Задание №4. Реализовать проверку пользователей
+        users_db = User.query.all()
 
+        for user in users_db:
+            if form_login == user.email and form_password == user.password:
+                session['user_email'] = user.email
+                return redirect('/index')
+
+        error = 'Неправильно указан пользователь или пароль'
+        return render_template('login.html', error=error)
      
     else:
         return render_template('login.html')
-
 
 
 @app.route('/reg', methods=['GET','POST'])
@@ -58,8 +69,9 @@ def reg():
         password = request.form['password']
         
         #Задание №3. Реализовать запись пользователей
-
-
+        user = User(email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
         
         return redirect('/')
     
@@ -70,8 +82,9 @@ def reg():
 #Запуск страницы с контентом
 @app.route('/index')
 def index():
-    #Задание №4. Сделай, чтобы пользователь видел тольуо свои карточки
-    cards = Card.query.order_by(Card.id).all()
+    #Задание №4. Сделай, чтобы пользователь видел только свои карточки
+    email = session.get('user_email')
+    cards = Card.query.filter_by(user_email=email).all()
     return render_template('index.html', cards=cards)
 
 #Запуск страницы c картой
@@ -95,7 +108,8 @@ def form_create():
         text =  request.form['text']
 
         #Задание №4. Сделай, чтобы создание карточки происходило от имени пользователя
-        card = Card(title=title, subtitle=subtitle, text=text)
+        email = session.get('user_email')
+        card = Card(title=title, subtitle=subtitle, text=text, user_email=email)
 
         db.session.add(card)
         db.session.commit()
